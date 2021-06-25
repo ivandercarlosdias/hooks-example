65ec3a787d3184ee49bed375dafacf20f500f411
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-06-25 13:48:11 -0300

    Criado a página e menu useState()

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -7,6 +7,7 @@ export default function MainNavbar(): JSX.Element {
             <ul>
                 <li>
                     <Link to="/">Home</Link>
+                    <Link to="/usestate">Hook UseState</Link>
                 </li>
             </ul>
         </nav>
