0d8d0b47da96d953174e7df7cc2d159863a02c53
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-06-25 17:15:22 -0300

    Fix menu useEffect()

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -8,10 +8,7 @@ export default function MainNavbar(): JSX.Element {
                 <li>
                     <Link to="/">Home</Link>
                     <Link to="/usestate">useState()</Link>
-<<<<<<< HEAD
                     <Link to="/useeffect">useEffect()</Link>
-=======
->>>>>>> 5bc2d1968d34be673c5ed77304f4bca99711f30a
                 </li>
             </ul>
         </nav>
