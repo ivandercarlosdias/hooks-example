7096fb901524069bd83c3868bf1a26b97bf0189f
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-06-25 15:51:19 -0300

    Ajuste na página useEffect()

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,10 +2,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../../modules/Home'
 import UseStateExample from '../../modules/UseState'
-<<<<<<< HEAD
 import UseEffectExample from '../../modules/UseEffect'
-=======
->>>>>>> 5bc2d1968d34be673c5ed77304f4bca99711f30a
 import NotFound from '../../modules/NotFound'
 
 import './style.scss'
@@ -20,12 +17,9 @@ export default function Content(): JSX.Element {
                 <Route path="/usestate">
                     <UseStateExample />
                 </Route>
-<<<<<<< HEAD
                 <Route path="/useeffect">
                     <UseEffectExample />
                 </Route>
-=======
->>>>>>> 5bc2d1968d34be673c5ed77304f4bca99711f30a
                 <Route path="*">
                     <NotFound />
                 </Route>
