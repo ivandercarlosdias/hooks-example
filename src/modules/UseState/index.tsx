40b3ea94633a68f906849566ce03fd618fb12db1
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-06-25 13:55:16 -0300

    Ajustes na página useState()

--- src/modules/UseState/index.tsx
+++ src/modules/UseState/index.tsx
@@ -1,27 +1,18 @@
+import { useState } from 'react'
+
 import PageTitle from '../../components/PageTitle'
 import SectionTitle from '../../components/SectionTitle'
 
 import './style.scss'
 
 export default function UseStateExample(): JSX.Element {
+    const [count, setCount] = useState(0)
+
     return (
         <>
-            <PageTitle title="Hook UseState" subtitle="Estado em componentes." />
+            <PageTitle title="Hook useState()" subtitle="Estado em componentes." />
             <SectionTitle title="Exercício #1"></SectionTitle>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ratione totam eveniet. Commodi
-                quisquam laborum eaque doloribus consequuntur? Deserunt sint autem aut repellat soluta consectetur
-                assumenda odio ex adipisci vitae.
-            </p>
-            <p>
-                Rem saepe exercitationem, id rerum deserunt quod laborum ratione qui quisquam architecto aliquid totam,
-                fugit iste blanditiis quis cumque corrupti dolorem minima eum eveniet recusandae perferendis fugiat.
-                Fugiat, voluptates explicabo.
-            </p>
-            <p>
-                Nulla sapiente, quia, hic, earum quae modi ex deleniti id ipsa et sunt quaerat repellat a excepturi
-                dignissimos iusto culpa veritatis! Numquam cupiditate quibusdam consequuntur amet illo sed iste neque.
-            </p>
+            <p>{count}</p>
         </>
     )
 }
